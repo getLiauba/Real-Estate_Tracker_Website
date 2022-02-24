cf898bcbade99d0d0f6143226ddb002ef3ad84f4
Andrew Liauba <Iamliauba@hotmail.com>
2022-02-23 19:09:57 -0500

    Added Card component and styling for it

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import Card from "./components/Card";
 function App() {
   return (
     <div className="App">
-      <Card />
+      <Card
+        title="For Sale"
+        description="Luxurious 9861 Sqft Custom Home On 0.93-Acre Lot W/Golf Course/Pond Views. Close To Amenities & Beach. 5+3 Bdrm W/Ensuites & W/I Closets In All Above Gr. Bdrms. 6.5 Baths, 5 Fp, Limestone/Marble/Hdwd Flring, Custom Kit W/Indust. Fridge, Gas Stove, Dbl Oven, Granite Counters, Lrg Brkfst Bar, Lrg Grt Rm W/Vaulted Ceilings, Custom Stone Fp, Formal Dr, Den, 5th Bdrm Over Grg(Gym). Ll W/Media Rm, Rec Rm & 3 Bdrms. 3 Car Grg, Fenced/Gated Yard. Mn Flr Sqft Is Ext.**** EXTRAS **** Inclusions: Dishwasher, Dryer, Garage Door Opener, Fridge, Stove, Washer, Window Coverings Exclusions: N/A Rentals: N/A (29461624)"
+      />
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,21 @@
 import React from "react";
+import "./Card.css";
 
 const Card = (props) => {
   return (
-    <div className="card">
-      <p>Hello</p>
+    <div className="card-container">
+      <div className="image-container">
+        <img src={require("../images/1.jpg")} height="100" />
+      </div>
+      <div className="card-content">
+        <div className="card-title">
+          <h3>{props.title}</h3>
+        </div>
+        <div className="card-description">{props.description}</div>
+        <div className="btn">
+          <button>View More</button>
+        </div>
+      </div>
     </div>
   );
 };
