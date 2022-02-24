58e5dc11889f1bf867191ff31c625d5a15a08082
Andrew Liauba <Iamliauba@hotmail.com>
2022-02-24 16:22:16 -0500

    Added button component and updated styles for the card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,23 +1,25 @@
-import React from "react";
 import "./Card.css";
+import Button from "./Button";
 
-const Card = (props) => {
+function Card(props) {
+  const classes = "card " + props.className;
   return (
-    <div className="card-container">
-      <div className="image-container">
-        <img src={require("../images/1.jpg")} height="100" />
-      </div>
+    <div className={classes}>
+      <h1>Hello World</h1>
+      <img src={require("../images/1.jpg")} height="100" width="185" />
+
       <div className="card-content">
         <div className="card-title">
           <h3>{props.title}</h3>
         </div>
         <div className="card-description">{props.description}</div>
+
         <div className="btn">
-          <button>View More</button>
+          <Button type="submit">View More</Button>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Card;
